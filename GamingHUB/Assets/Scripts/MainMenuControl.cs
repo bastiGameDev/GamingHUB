b6f8d5ce518c496870ab83fe7b2f242d809920f7
bastiGameDev <107358763+bastiGameDev@users.noreply.github.com>
2022-09-18 09:06:54 +0300

    Добавлен текст
    Добавлен текст в выбор меню жанра игр

--- GamingHUB/Assets/Scripts/MainMenuControl.cs
+++ GamingHUB/Assets/Scripts/MainMenuControl.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class MainMenuControl : MonoBehaviour
 {
     [SerializeField] private Transform movingPanel;
-    [SerializeField] private Transform movingText;
+    [SerializeField] private Transform movingTextGameHub;
+    [SerializeField] private Transform movingTextAboutGame;
     [SerializeField] private Transform buttonGo;
     private readonly Vector3 _finalPositionRight = new Vector3(-169f, 0.03445524f, 96f);
     private readonly Vector3 _finalPositionLeft = new Vector3(198.1f, 0.03445524f, 96f);
@@ -21,7 +22,7 @@ public class MainMenuControl : MonoBehaviour
     {
         await Task.Delay(500);
         buttonGo.DOMove((new Vector3(0f, 0f, 96f)), 2f).SetEase(Ease.OutBack);
-        movingText.DOMove((new Vector3(0f, 20f, 96f)), 2f).SetEase(Ease.OutBack);
+        movingTextGameHub.DOMove((new Vector3(0f, 20f, 96f)), 2f).SetEase(Ease.OutBack);
         await movingPanel.DOMove(_finalPositionRight, 40f).AsyncWaitForCompletion();
         await movingPanel.DOMove(_finalPositionLeft, 80f).AsyncWaitForCompletion();
     }
@@ -29,9 +30,9 @@ public class MainMenuControl : MonoBehaviour
     public async void ButtonGoClick()
     {
         buttonGo.DOMove((new Vector3(0f, 70f, 96f)), 1f).SetEase(Ease.OutBack);
-        movingText.DOMove(new Vector3(0, 50f, 96f), 2f).SetEase(Ease.OutBack);
+        movingTextGameHub.DOMove(new Vector3(0, 50f, 96f), 2f).SetEase(Ease.OutBack);
         musicBox.Play();
         await Task.Delay(600);
-        //startPanel.SetActive(false);
+        movingTextAboutGame.DOMove((new Vector3(0f, 20f,96f)), 1f).SetEase(Ease.OutBack);
     }
 }
